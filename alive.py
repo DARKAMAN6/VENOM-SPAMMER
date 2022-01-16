56be99483809a3ac2227f7363b1a5901cdb8f38a
DARKAMAN6 <89340128+DARKAMAN6@users.noreply.github.com>
2022-01-16 19:12:53 +0530

    Update alive.py

--- alive.py
+++ alive.py
@@ -1,3 +1,14 @@
 import os
 import re
 from telethon import events, Button
+from telethon import TelegramClient, events
+from telethon.tl import functions
+
+
+
+
+
+
+
+
+@idk.on(events.NewMessage(incoming=True, pattern=r"\*delayspam"))
